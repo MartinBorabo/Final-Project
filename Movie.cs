6f0e1713235ecd1d854365fc7cc0f6c911a5bb8a
Martin <borabopaul4@gmail.com>
2026-06-12 01:00:47 +1200

    simple Movie class inheriting MovieDetails

---
+++ Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class Movie : MovieDetails
+    {
+        //fields
+        private string dvdCount;
+
+        //properties
+        public string DvdCount
+        {
+            get { return dvdCount; }
+            set { dvdCount = value; }
+        }
+
+        //constructor
+        public Movie(string title, string genre, int year, string dvdCount) : base(title, genre, year)
+        {
+            DvdCount = dvdCount;
+        }
+
+    }
+}
