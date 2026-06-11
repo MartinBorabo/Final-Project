fe4f636facda47b83cda16e25059f0ece11ab1ef
Martin <borabopaul4@gmail.com>
2026-06-12 00:56:30 +1200

    Movie Details class

---
+++ MovieDetails.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal abstract class MovieDetails
+    {
+        //fieds
+        private string title;
+        private string genre;
+        private int year;
+
+        //properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        //constructor
+        public MovieDetails(string title, string genre, int year)
+        {
+            Title = title;
+            Genre = genre;
+            Year = year;
+        }
+
+
+    }
+}
