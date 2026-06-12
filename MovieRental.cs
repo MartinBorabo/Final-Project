17be558f91adf613d9cf52debdc8c29b9b02b672
Martin <borabopaul4@gmail.com>
2026-06-12 19:39:35 +1200

    added new class and basic list to divide the workload

---
+++ MovieRental.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class MovieRental
+    {
+        // A list to store movies in the rental system
+        private List<Movie> movies = new List<Movie>();
+
+        
+
+        // ------------------------
+        // LIST  ALL MOVIES
+        // ------------------------
+
+
+        // ------------------------
+        // SEARCH ALL MOVIES
+        // ------------------------
+
+
+        // ------------------------
+        // RENT A MOVIE
+        // ------------------------
+
+
+        // ------------------------
+        // ADD A MOVIE (Admin only)
+        // ------------------------
+        
+
+        // ------------------------  
+        // REMOVE A MOVIE (Admin only)
+        // ------------------------ 
+
+
+        // ------------------------
+        // UPDATE A MOVIE (Admin only)
+        // ------------------------
+
+    }
+}
